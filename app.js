5cac5eea55b0cc0897812b53cb4eee5246398391
Florsecco <florenciasecco3@gmail.com>
2023-10-31 10:01:46 -0300

    corrigiendo error de ortografia

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORTH = 3010
+const PORT = 3010
 
 app.use( express.static(__dirname + '/public'))
 
-app.listen(PORTH, ()=>console.log("Servidor corriendo en el puerto "+PORTH))
+app.listen(PORT, ()=>console.log("Servidor corriendo en el puerto "+PORT))
 
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
