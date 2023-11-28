b95022c56f1a8757e0bdaede3b32d2f38d762113
euzzante <euzzante@bse.com.ar>
2023-11-28 11:46:13 -0300

    Se modifico el createProduct

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,20 @@
 const express = require("express");
-
+// const { join } = require("path");
+const path = require('path');
 const router = express.Router();
+const multer = require('multer');
+// const upload = multer({ dest: '.public/img/products/' })
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../public", '/img/products'));
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({ storage: storage });
 
 const productController = require("../controllers/productController");
 
@@ -13,5 +27,5 @@ router.get("/editProduct", productController.editProduct);
 router.get("/:id", productController.productDetail);
 router.get("/:id/edit", productController.productEdit);
 
-router.post("/", productController.create);
+router.post("/", upload.single('product'), productController.create);
 module.exports = router;
