dbbf1183b5688e4b92e182f474a8db6e2fdcd707
Florsecco <florenciasecco3@gmail.com>
2023-11-28 16:06:43 -0300

    cambiando el products

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,22 @@
 const {index, findOne} = require("../models/product.model");
 
+const toThousand = (numero) =>{
+    const opciones = {
+        style: 'decimal',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    };
+    const formatoNumero = new Intl.NumberFormat('de-DE', opciones);
+    return formatoNumero.format(numero);
+}
+
+
 const mainController = {
   home: (req, res) => {
-    res.render("home", { products: index() });
+    const products = index();
+    const offers = products.filter((product)=>product.discount!=0)
+    const featured = products.filter((product)=>product.discount==0)
+    res.render("home", { offers, featured, toThousand });
   },
 };
 
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,23 @@
 const { findOne, index, create } = require("../models/product.model");
 const path = require('path');
+const toThousand = (numero) =>{
+  const opciones = {
+      style: 'decimal',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+  };
+  const formatoNumero = new Intl.NumberFormat('de-DE', opciones);
+  return formatoNumero.format(numero);
+}
 
 const productController = {
   index: (req, res) => {
     const products = index();
+    const offers = products.filter((product)=>product.discount!=0)
+    const featured = products.filter((product)=>product.discount==0)
     console.log(products);
     // const offers
-    res.render("./products/products", { products })
+    res.render("./products/products", { offers,featured,toThousand })
   },
   cart: (req, res) => {
     res.render("./products/cart");
@@ -18,7 +29,7 @@ const productController = {
     if (product === undefined)
       res.redirect('../not-found');
     console.log(product);
-    res.render("./products/productDetail", { product });
+    res.render("./products/productDetail", { product, toThousand });
   },
   createProduct: (req, res) => {
     res.render("./products/createProduct");
