673188e4b0367fc1f83abc4191cd43586bd05115
Florsecco <florenciasecco3@gmail.com>
2023-12-18 15:43:24 -0300

    agregando multer

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const upload = require('../middlewares/userMulterMiddleware')
 
 const router = express.Router();
 
+// Register //
+
 router.get("/register", userController.register);
+
+// Login //
 
 router.get("/login", userController.login);
 
+// Profile //
+
 router.get("/profile/:id", userController.profile);
+
+// Edit //
 
 router.get("/edit/:id", userController.edit);
 
