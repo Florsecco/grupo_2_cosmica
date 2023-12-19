2cdefde042fa6c9a1547cd0c29d2b00f4eca927e
gabyfinn <gabrieluzzante@gmail.com>
2023-12-18 22:24:56 -0300

    Se agrego express-validator y session para el login de un usuario

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override')
+const morgan = require('morgan');
+const cookies = require('cookie-parser');
+const session = require('express-session');
+
 const PORT = 3010;
 
 const mainRouter = require('./routes/main.js');
 const userRouter = require('./routes/user.js');
 const productRouter = require('./routes/product.js');
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/../public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(cookies());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}));
 
 app.listen(PORT, () => console.log("Servidor corriendo en el puerto " + PORT));
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,9 @@
-const { index, findOne } = require("../models/user.model");
+const { index, findOne, findByEmail } = require("../models/user.model");
+const bcrypt = require('bcryptjs');
+const salt = 10;
+const { validationResult } = require('express-validator');
+
+
 
 const fs = require('fs')
 
@@ -11,17 +16,39 @@ const userController = {
   login: (req, res) => {
     res.render("./users/login");
   },
-  profile: (req,res) =>{
-    const id  = req.params.id;
-    const user = findOne(id);
+  profile: (req, res) => {
+    const user = req.session.userLogged;
     if (user === undefined)
       res.redirect('../not-found');
-    res.render("./users/profile", {user})
+    res.render("./users/profile", { user })
   },
-  edit: (req,res)=>{
+  edit: (req, res) => {
     const { id } = req.params;
     const user = findOne(id);
-    res.render("./users/editProfile", {user})
+    res.render("./users/editProfile", { user })
+  },
+  processLogin: (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return res.render('./users/login', {
+        errors: errors.mapped(),
+        old: req.body
+      })
+    }
+
+    const user = findByEmail(req.body);
+    delete user.password;
+    req.session.userLogged = user;
+    if (!user)
+      return res.render('./users/login', {
+        errors: {
+          msg: 'Credenciales incorrectas.'
+        },
+        old: req.body
+      });
+
+    res.redirect(`profile`);
   }
 };
 
